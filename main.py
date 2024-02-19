c9f00d195d1a51fefaa4975bdab132839bf0761a
BH3Panzer <clementcattin74@gmail.com>
2024-02-19 19:48:26 +0100

    removed OOB for app and draw title

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pyxel as px
 WIDTH = 512
 HEIGHT = 256
-class App:
-    def __init__(self):
-        px.init(WIDTH, HEIGHT)
-        px.mouse(True)
-        px.run(self.update, self.draw)
-        self.state = "main_menu"
+px.init(WIDTH, HEIGHT)
+px.mouse(True)
+px.load("res.pyxres")
+state = "main_menu"
 
-    def update(self):
-        pass
+def update():
+    pass
 
-    def draw(self):
-        px.cls(0)
+def draw():
+    px.cls(0)
+    if state == "main_menu":
+        px.blt(int(WIDTH/8)*3, 0, 0, 8, 0, 39, 24, colkey=0)
 
-App()
+px.run(update, draw)
