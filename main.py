e5a5030905c2dfe3a674f2ef60658038fddb8270
raphael.georget <raphael.georget@TLPU002.etab.local>
2024-04-02 12:26:24 +0200

    lol

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pyxel as px
-WIDTH = 512
-HEIGHT = 256
+WIDTH = 256
+HEIGHT = 128
 px.init(WIDTH, HEIGHT)
 px.mouse(True)
 px.load("res.pyxres")
 state = "main_menu"
+
+#son  ton:Pulse,Triangle,Square,Noise  efx:F; la note s'arrête instant, S;relie les notes, Vibrato, Normal
 
 px.playm(0, loop=True)
 class Player:
@@ -21,6 +23,7 @@ def update():
 def draw():
     px.cls(0)
     if state == "main_menu":
-        px.blt(int(WIDTH/8)*3, 0, 0, 8, 0, 145, 23, colkey=0)
+        px.blt(int(WIDTH/2)-72, 0, 0, 8, 0, 145, 24, colkey=0)
+        px.text(int(WIDTH/2),int(HEIGHT/2),"abcdefghijklmno pqrstuvwxyz",1) #6 hauteur, 3 largeur
 
 px.run(update, draw)
