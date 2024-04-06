e3de1d21b727aee11ac432a1349d230f7b84f1db
Fraii <raphael.georget01@gmail.com>
2024-04-06 09:47:33 +0200

    affichage entitées

---
+++ frames.py
@@ -0,0 +1,16 @@
+
+"""
+Pour chaque partie du corps on a les coordonnées du point haut gauche, celle du coin bas droit et celle du décalage (0,0 si rien)
+transparence: indique la couleur de transparence
+defaut: emplacement du sprite entier par défaut (dans les parties du corps élements 0 = par défaut)
+"""
+frames = {
+        "perso": 
+        {
+            "transparence":7,
+            "defaut":[0,40,15,55],
+            "pied":[[6,53,10,55]],
+            "corps":[[4,47,12,52],[17,41,25,46],[27,41,35,46]],
+            "tete":[[5,41,11,46]]
+        }
+    }
