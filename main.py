757a377446ec86e4d1e0d0785459d9df0ce9549b
raphael.georget <raphael.georget@TLPU002.ETAB.LOCAL>
2024-05-02 15:19:31 +0200

    save selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyxel as px
+from os import listdir
 WIDTH = 256
 HEIGHT = 128
 fps = 30
@@ -15,6 +16,10 @@ cristalTick = [0,0]
 cristalClock = [[0,3],[8,5],[9,16],[8,5],[0,3],[7,5],[6,16],[7,5]]
 allEntities = []
 tick = 0 #timer pour des animations trop stylé 
+mapA = None #nom de la map actuel
+listSaves = listdir("saves/") #liste les fichiers de sauvegardes
+cursor = 0 #utiliser pour indiquer la position dans un menu déroulant
+saveLoad = None #nom du fichier de sauvegarde actuellement utiliser
 
 from frames import *
 
@@ -64,6 +69,26 @@ def drawEntitie(skin, x, y, name, reverse = False):
         px.blt(x + frames[name][i[0]][0][0] - frames[name]["defaut"][0] + ((sprite[4] * (-1 if reverse else 1)) if len(sprite)>=5 else 0), y + frames[name][i[0]][0][1] - frames[name]["defaut"][1] + (sprite[5] if len(sprite)>=6 else 0)
             , frames[name]["image"], sprite[0], sprite[1], (sprite[2]-sprite[0]+1) * (-1 if reverse else 1), sprite[3]-sprite[1]+1, colkey=frames[name]["transparence"])
 
+def drawFichier(x,y,name):
+    px.rect(x,y,236,25,1)
+    px.text(x+5,y+5,name[0:-4],9)
+
+def readSaves(saves):
+    pass
+
+def animStyle(col = 1): #anim trop stylée avec des tit' rectangles
+    if tick > 0:
+        for x in range(int(tick)):
+            for y in range(13):
+                i = x - y
+                tickA = tick - y
+                if i >= 0:
+                    if 4-(tickA-i) >= 0:
+                        temp = 4-(tickA-i)
+                    else:
+                        temp = 0
+                    px.rect(i*10+temp,y*10+temp,(tickA-i+1)*2 if tickA-i<4 else 10,(tickA-i+1)*2 if tickA-i<4 else 10,col)
+
 #détecte si un point est dans un rectangle
 def collidpoint(point, rect):
     if point[0] >= rect[0] and point[0] <= rect[2] and point[1] >= rect[1] and point[1] <= rect[3]:
@@ -73,8 +98,8 @@ def collidpoint(point, rect):
 
 # update game
 def update():
-    global cristal,cristalTick,tick,state
-    if state == "title_menu":
+    global cristal,cristalTick,tick,state,cursor
+    if state == "title_menu": #menu avec uniquement le titre
         cristalTick[1] += 1
         if cristalTick[1] == cristalClock[cristalTick[0]][1]:
             cristalTick[0] += 1
@@ -90,17 +115,31 @@ def update():
             if tick == 15:
                 state = "main_menu"
                 tick = 0
-    elif state == "main_menu":
+    elif state == "main_menu": #menu principal
+        if px.btnp(px.MOUSE_BUTTON_LEFT):
+            if collidpoint([px.mouse_x, px.mouse_y], [3,3,18,18]):
+                px.quit()
+            elif collidpoint([px.mouse_x, px.mouse_y], [112,56,144,72]):
+                state = "selFichier"
+    elif state == "selFichier": #menu de séléction de fichier de sauvegarde
         if tick != 0:
             tick += 0.7
             if tick > 42:
                 state = "ingame"
                 tick = 0
-        elif px.btnp(px.MOUSE_BUTTON_LEFT):
-            if collidpoint([px.mouse_x, px.mouse_y],[3,3,18,18]):
-                px.quit()
-            elif collidpoint([px.mouse_x, px.mouse_y],[112,56,144,72]):
-                tick += 1
+        else:
+            if px.btnp(px.MOUSE_BUTTON_LEFT):
+                if collidpoint([px.mouse_x, px.mouse_y], [3,3,18,18]):
+                    state = "main_menu"
+                else:
+                    for i in range(len(listSaves) if len(listSaves)<=3 else 3):
+                        if collidpoint([px.mouse_x, px.mouse_y], [10,30+i*34,236,55+i*34]):
+                            saveLoad = listSaves[i+cursor]
+                            tick += 1
+            if px.btnp(px.KEY_S, 30, 10) or px.btnp(px.KEY_DOWN, 30, 10):
+                cursor += 1
+            if (px.btnp(px.KEY_Z, 30, 10) or px.btnp(px.KEY_UP, 30, 10)) and cursor != 0:
+                cursor -= 1
 
 # draw game
 def draw():
@@ -120,17 +159,15 @@ def draw():
         px.blt(237,3,0,168,0,16,16,colkey=0) #bouton option
         px.blt(112,56,0,184,0,32,16,colkey=0) #bouton play
         px.text(3,119,"created by BH3Panzer and Fraii",1)
-        if tick > 0:
-            for x in range(int(tick)):
-                for y in range(13):
-                    i = x - y
-                    tickA = tick - y
-                    if i >= 0:
-                        if 4-(tickA-i) >= 0:
-                            temp = 4-(tickA-i)
-                        else:
-                            temp = 0
-                        px.rect(i*10+temp,y*10+temp,(tickA-i+1)*2 if tickA-i<4 else 10,(tickA-i+1)*2 if tickA-i<4 else 10,1)
+    elif state == "selFichier":
+        px.bltm(0,0,0,832,384,256,128)
+        px.blt(int(WIDTH/2)-72, 0, 0, 8, 0, 144, 24, colkey=0)
+        px.blt(3,3,0,152,0,16,16,colkey=0) #bouton quitter
+        for i in range(len(listSaves) if len(listSaves)<=3 else 3):
+            drawFichier(10,30+i*34,listSaves[i+cursor])
+            if collidpoint([px.mouse_x, px.mouse_y], [10,30+i*34,236,55+i*34]):
+                px.rectb(9,29+i*34,238,27,10)
+        animStyle(9)
 
 # created some entities for title menu
 newEntitie("scientifique", {"pied":0,"corps":0,"tete":0,"oeil":0}, [81,56], False)
